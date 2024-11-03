f688ff55e67b5f9909c82fabfbdad695dad771c5
irini-git <irina.nikolkina@gmail.com>
2024-11-03 21:25:38 +0200

    Save data as self

--- src/data_manager.py
+++ src/data_manager.py
@@ -9,7 +9,7 @@ PDF_LOCAL_FILE = './data/2024-gouvernement-francais-etranger-rapport.pdf'
 
 class ReportData:
     def __init__(self):
-        self.load_data()
+        self.df = self.load_data()
 
     def get_current_location(self):
         """
@@ -28,11 +28,29 @@ class ReportData:
         with pdfplumber.open(PDF_LOCAL_FILE) as pdf:
 
             # Extract tables using list comprehension
-            # [ expression for item in list if conditional ]
+            # [expression for item in list if conditional]
             pdf_tables = [page.extract_table() for page in pdf.pages if page.page_number in [151,152, 153, 154, 155]]
 
         # Unpack lists
         pdf_tables = list(itertools.chain(*pdf_tables))
 
-        print(pdf_tables)
-        print(pdf_tables[3:])
+        # Create a dataframe based on pdf table
+        df = pd.DataFrame(pdf_tables[3:])
+        df.columns = ['Rang', 'Country', 'Population 2023', 'Change 2023/2022 (%)', 'col1', 'col2']
+        df.drop(['col1', 'col2'], axis=1, inplace=True)
+
+
+
+        # Change datatypes
+        # Rang, population in 2023; and 2023/2022 are numeric columns
+        # - population : remove white space (a thousand separator)
+        # - change 2023/2022: remove percentage and replace separator
+        df['Population 2023'] = df['Population 2023'].str.replace(' ', '')
+        df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace('%', '')
+        df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace(',', '.')
+
+        # Convert columns  to numeric
+        columns_numeric = ['Rang', 'Population 2023', 'Change 2023/2022 (%)']
+        df[columns_numeric] = df[columns_numeric].apply(pd.to_numeric)
+
+        return df

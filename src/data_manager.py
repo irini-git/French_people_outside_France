12326b042d2f98db3b296086a9ad1cedf937cf7b
irini-git <irina.nikolkina@gmail.com>
2024-11-03 21:31:09 +0200

    Load data from local PDF or from pickle if exists

--- src/data_manager.py
+++ src/data_manager.py
@@ -6,6 +6,7 @@ import itertools
 # URL from which PDF to be downloaded
 PDF_URL = 'https://francais-du-monde.org/wp-content/uploads/2022/11/2024-gouvernement-francais-etranger-rapport.pdf'
 PDF_LOCAL_FILE = './data/2024-gouvernement-francais-etranger-rapport.pdf'
+INPUT_DATA_PICKLE = './data/df.pickle'
 
 class ReportData:
     def __init__(self):
@@ -20,37 +21,47 @@ class ReportData:
 
 
     def load_data(self):
-        """ Loads data from local pdf file and transforms to dataframe
-        :return: dataframe with data
+        """ Loads data from a pickle file (if exists) or from a local PDF file
+         and transforms to dataframe.
+         If a pickle file does not exist, then it will be created.
+        :return:  with cleaned data
         """
 
-        # Open the PDF file and extract tables
-        with pdfplumber.open(PDF_LOCAL_FILE) as pdf:
+        # Check if the file exists
+        if os.path.exists(INPUT_DATA_PICKLE):
+            print("Load from pickle file.")
+            return pd.read_pickle(INPUT_DATA_PICKLE)
 
-            # Extract tables using list comprehension
-            # [expression for item in list if conditional]
-            pdf_tables = [page.extract_table() for page in pdf.pages if page.page_number in [151,152, 153, 154, 155]]
+        else:
+            print("Load data from local PDF and pickle.")
 
-        # Unpack lists
-        pdf_tables = list(itertools.chain(*pdf_tables))
+            # Open the PDF file and extract tables
+            with pdfplumber.open(PDF_LOCAL_FILE) as pdf:
 
-        # Create a dataframe based on pdf table
-        df = pd.DataFrame(pdf_tables[3:])
-        df.columns = ['Rang', 'Country', 'Population 2023', 'Change 2023/2022 (%)', 'col1', 'col2']
-        df.drop(['col1', 'col2'], axis=1, inplace=True)
+                # Extract tables using list comprehension
+                # [expression for item in a list if conditional]
+                pdf_tables = [page.extract_table() for page in pdf.pages if page.page_number in [151,152, 153, 154, 155]]
 
+            # Unpack lists
+            pdf_tables = list(itertools.chain(*pdf_tables))
 
+            # Create a dataframe based on PDF table
+            df = pd.DataFrame(pdf_tables[3:])
+            df.columns = ['Rang', 'Country', 'Population 2023', 'Change 2023/2022 (%)', 'col1', 'col2']
+            df.drop(['col1', 'col2'], axis=1, inplace=True)
 
-        # Change datatypes
-        # Rang, population in 2023; and 2023/2022 are numeric columns
-        # - population : remove white space (a thousand separator)
-        # - change 2023/2022: remove percentage and replace separator
-        df['Population 2023'] = df['Population 2023'].str.replace(' ', '')
-        df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace('%', '')
-        df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace(',', '.')
+            # Change datatypes
+            # Rang, population in 2023; and 2023/2022 are numeric columns
+            # - population : remove white space (a thousand separator)
+            # - change 2023/2022: remove percentage and replace separator
+            df['Population 2023'] = df['Population 2023'].str.replace(' ', '')
+            df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace('%', '')
+            df['Change 2023/2022 (%)'] = df['Change 2023/2022 (%)'].str.replace(',', '.')
 
-        # Convert columns  to numeric
-        columns_numeric = ['Rang', 'Population 2023', 'Change 2023/2022 (%)']
-        df[columns_numeric] = df[columns_numeric].apply(pd.to_numeric)
+            # Convert columns  to numeric
+            columns_numeric = ['Rang', 'Population 2023', 'Change 2023/2022 (%)']
+            df[columns_numeric] = df[columns_numeric].apply(pd.to_numeric)
+
+            df.to_pickle(INPUT_DATA_PICKLE)
 
         return df
